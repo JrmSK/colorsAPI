41e8add268d382874527a1c596905b0387966614
JrmSK <jeremy.skornik@gmail.com>
2019-01-07 00:54:41 +0200

    exercise done

---
+++ Exercise2/colors/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+import json
+
+colors_list = ['blue', 'orange', 'green', 'black']
+
+
+def index(request):
+    return HttpResponse("Welcome to colors app.")
+
+
+def list(request):
+    return render(request, 'colors/list.html', {'colors': colors_list})
+
+
+def add(request):
+    color_to_add = request.GET.get('color')
+    if color_to_add in colors_list:
+        response_data = {'message': 'color {} already in the list'.format(color_to_add)}
+        return HttpResponse(json.dumps(response_data), content_type="application/json", status=409)
+    else:
+        colors_list.append(color_to_add)
+        response_data = {'colors': colors_list,
+                         'message': 'color {} successfully added to the list'.format(color_to_add)}
+    return render(request, 'colors/list.html', {'colors': colors_list}, status=201)
+
+
+def get(request):
+    color = request.GET.get('color')
+    if color in colors_list:
+        return HttpResponse("{}".format(color))
+    else:
+        return HttpResponseNotFound("no such color {}".format(color))
